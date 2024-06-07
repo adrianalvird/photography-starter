56487667201757aab78e6fab3895ae641c4fcefa
Adrian Alvird <51330833+adrianalvird@users.noreply.github.com>
2024-06-07 05:27:56 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./components/Header";
 import AnimRoutes from "./components/AnimRoutes";
 import MobileNav from "./components/MobileNav";
 import Socials from "./components/Socials";
-import {useContext} from 'react';
+import { useContext } from "react";
 import { CursorContext } from "./context/CursorContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const App = () => {
+  const { cursorVariants } = useContext();
   useContext(CursorContext);
   return (
-    <Router>
-      <Header />
-      <AnimRoutes />
-    </Router>
-    <div className="w-[32px] h-[32px] bg-primary fixed top-0 left-0 pointer-events-none z-50"></div>
+    <Fragment>
+      <Router>
+        <Header />
+        <AnimRoutes />
+      </Router>
+      <motion.div
+        variants={cursorVariants}
+        className="w-[32px] h-[32px] bg-primary fixed top-0 left-0 pointer-events-none z-50"
+      ></motion.div>
+    </Fragment>
   );
 };
 
--- src/context/CursorContext.js
+++ src/context/CursorContext.js
@@ -9,12 +9,24 @@ const CursorProvider = ({ children }) => {
   });
   useEffect(() => {
     const move = (e) => {
-      setCursorPos({ x: e.clientX, y: clientY });
+      setCursorPos({ x: e.clientX, y: e.clientY });
     };
     window.addEventListner("mousemove", move);
+    return () => {
+      window.removeEventListener("mousemove", move);
+    };
   });
+
+  const cursorVariants = {
+    default: {
+      x: cursorPos.x - 16,
+      y: cursorPos.y - 16,
+      backgoundColor: "#0e1112",
+    },
+  };
+
   return (
-    <CursorContext.Provider value={"this is the cursor"}>
+    <CursorContext.Provider value={{cursorVariants}}>
       {children}
     </CursorContext.Provider>
   );
